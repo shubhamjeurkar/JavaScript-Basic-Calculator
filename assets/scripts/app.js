7d71d82ddb4d103c3e731ee005e7f48b7adf068f
Shubham Jeurkar <shubhamjeurkar11@gmail.com>
2023-05-08 21:14:22 +0530

    Added Functions and event listeners

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,1 +1,22 @@
-alert('This Works!');
+let currentResult=0;
+
+//currentResult = currentResult + 10 * 3 / 2 -1;
+
+function add(num1 , num2){
+    const enteredNum = parseInt(userInput.value);
+    const calcDesciption = `${currentResult} + ${enteredNum}`;
+    currentResult += enteredNum;
+    outputResult(currentResult, calcDesciption);
+}
+
+function subtract(num1 , num2){
+    const enteredNum = parseInt(userInput.value);
+    const calcDesciption = `${currentResult} - ${enteredNum}`;
+    currentResult -= enteredNum;
+    outputResult(currentResult , calcDesciption);
+}
+
+//currentResult=add(2,5);
+addBtn.addEventListener('click' , add);
+subtractBtn.addEventListener('click' , subtract);
+
